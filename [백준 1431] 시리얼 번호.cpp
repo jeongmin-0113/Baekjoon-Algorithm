c06538487c80628a4a73c30ddb470b4d08d6c5ab
Jeongmin Park <60566546+jeongmin-0113@users.noreply.github.com>
2022-04-09 22:47:42 +0900

    20220409

--- [백준 1431] 시리얼 번호.cpp
+++ [백준 1431] 시리얼 번호.cpp
@@ -1,46 +1,46 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 #include <string>
+#include <algorithm>
+#include <vector>
 using namespace std;
 
-bool my_compare(const string &a, const string &b)
-{
-	if (a.size() != b.size()) return a.size() < b.size();
-	else
-	{
-		int a_num = 0, b_num = 0;
-		for (int i = 0; i < a.size(); ++i)
-		{
-			if (isdigit(a[i])) a_num += a[i] - '0';
-		}
-		for (int i = 0; i < b.size(); ++i)
-		{
-			if (isdigit(b[i])) b_num += b[i] - '0';
-		}
-
-		if (a_num != b_num) return a_num < b_num;
-		else
-		{
-			return a < b;
-		}
+int cnt(string s, int size) {
+	int sum = 0;
+	for (int i = 0; i < size; ++i) {
+		if (isdigit(s[i])) sum += s[i] - '0';
 	}
+	return sum;
 }
 
-int main()
-{
-	int n;
-	cin >> n;
-	vector <string> serial_num;
-
-	for (int i = 0; i < n; ++i)
-	{
-		string tmp;
-		cin >> tmp;
-		serial_num.push_back(tmp);
+bool compare(string a, string b) {
+	int aSize = a.size();
+	int bSize = b.size();
+	if (aSize != bSize) return aSize < bSize;
+	else {
+		int aSum = cnt(a, aSize);
+		int bSum = cnt(b, bSize);
+		if (aSum != bSum) return aSum < bSum;
+		else return a < b;
 	}
-	sort(serial_num.begin(), serial_num.end(), my_compare);
+	
+}
 
-	for (int i = 0; i < n; ++i)
-		cout << serial_num[i] << endl;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int n;
+	vector <string> v;
+	cin >> n;
+	for (int i=0; i<n; ++i) {
+		string s;
+		cin >> s;
+		v.push_back(s);
+	}
+	sort(v.begin(), v.end(), compare);
+
+	for (int i = 0; i < n; ++i) {
+		cout << v[i] << '\n';
+	}
+	return 0;
 }
