ef63be3aea336cc7ea04d68c8b66143418646243
Jeongmin Park <60566546+jeongmin-0113@users.noreply.github.com>
2022-07-11 22:48:42 +0900

    20220711
    2차 풀이

--- [백준 1074] Z.cpp
+++ [백준 1074] Z.cpp
@@ -8,26 +8,27 @@ void func(int length, int r, int c) {
 	int a = length / 2;
 	a *= a; // 한 사분면의 넓이 계산
 
-	int mid = (length / 2) - 1; // 0번째부터 시작하므로 1 빼줌
+	int mid = length / 2;
 
-	if (r <= mid && c <= mid) {
+	// 0번째부터 시작하므로 mid보다 작은 경우 / 크거나 같은 경우로 사분면이 나뉨
+	if (r < mid && c < mid) {
 		// 1사분면
-		func(mid + 1, r, c);
+		func(mid, r, c);
 	}
-	else if (r <= mid && c > mid) {
+	else if (r < mid && c >= mid) {
 		// 2사분면
 		ans += a;
-		func(mid + 1, r, c - (mid + 1));
+		func(mid, r, c - mid);
 	}
-	else if (r > mid && c <= mid) {
+	else if (r >= mid && c < mid) {
 		// 3사분면
 		ans += a * 2;
-		func(mid + 1, r - (mid + 1), c);
+		func(mid, r - mid, c);
 	}
 	else {
 		// 4사분면
 		ans += a * 3;
-		func(mid + 1, r - (mid + 1), c - (mid + 1));
+		func(mid, r - mid, c - mid);
 	}
 }
 
